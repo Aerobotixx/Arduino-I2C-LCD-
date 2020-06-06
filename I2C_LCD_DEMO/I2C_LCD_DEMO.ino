0211fc2e3d2ccae661494108a4cf115212498fba
Aerobotix <66065117+Aerobotixx@users.noreply.github.com>
2020-06-06 18:21:19 +0530

    Add files via upload

---
+++ I2C_LCD_DEMO/I2C_LCD_DEMO.ino
@@ -0,0 +1,34 @@
+//*****************READ INSTRUCTIONS CAREFULLY******************
+//AFTER FINDING THE I2C ADDRESS, INSTALL THE FOLLOWING LIBRARY AS SHOWN IN VIDEO.
+//IF YOU FACE ANY PROBLEM IN INSTALLING THE LIBARY, DOWNLOAD & INSTALL IT MANUALLY.
+//DOWNLOAD THE LIBRARY FROM THIS LINK : https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+//GET OTHER EXAPMLE CODES HERE : https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library/tree/master/examples
+//**************************************************************
+
+#include <LiquidCrystal_I2C.h>    //importing library. make sure you have installed it
+
+//creating object of LCD with I2C address & LCD type
+LiquidCrystal_I2C lcd(0x27,16,2);  //(LCD address, lcd column, lcd row)
+//LiquidCrystal_I2C lcd(your address,20,4);  //If you have 20 x 4 LCD, then use this line with yor LCD address
+
+void setup() 
+{
+  // put your setup code here, to run once:
+   lcd.init();  //initialize LCD
+   lcd.backlight();   //You can control the backlight by this function. use nobacklight() to turn off the backlight
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+  /*give the initial position to display text
+  lcd.setCursor(0,0) the first 0 is out of 16 & second 0 is out of 2 (as of 16x2 LCD)*/
+  lcd.clear();
+  lcd.setCursor(0,0); //Data on line number 1 with initial position 0 
+  
+  /*data whichever you want to display. It should be only 16 characters long */
+  lcd.print("WELCOME");   
+  lcd.setCursor(0,1);   //Data on line number 2 with initial position 0
+  lcd.print("THIS IS 16x2 LCD"); 
+}
---
+++ I2C_Scanner/I2C_Scanner.ino
@@ -0,0 +1,41 @@
+//I2C Scanner program to find out the I2C address of device
+//Do not make any changes in this code simply upload this code to your Arduino
+//Connect your I2C device to arduino properly & open the serial monitor
+//After uploading code, dont forget to choose baudrate as 115200 on serial monitor
+
+#include <Wire.h>
+
+void setup() {
+  Serial.begin (115200);  // do not change this
+
+  // Leonardo: wait for serial port to connect
+  while (!Serial)
+    {
+    }
+
+  Serial.println ();
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+ 
+  Wire.begin();
+  for (byte i = 1; i < 120; i++)
+  {
+    Wire.beginTransmission (i);
+    if (Wire.endTransmission () == 0)
+      {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);
+      Serial.println (")");
+      count++;
+      delay (1);  // maybe unneeded?
+      } // end of good response
+  } // end of for loop
+  Serial.println ("Done.");
+  Serial.print ("Found ");
+  Serial.print (count, DEC);
+  Serial.println (" device(s).");
+}  // end of setup
+
+void loop() {}
